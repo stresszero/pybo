fc27df7b0f40cd50c0ff963a957ebbbb1a28e74d
stresszero <restouri@gmail.com>
2022-05-05 10:06:46 +0900

    add template html

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
@@ -9,7 +9,9 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 
-def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+def detail(request, question_id):  # url 매핑시 저장된 question_id로 함수호출
+    # question = Question.objects.get(id=question_id)
+    # pk는 primary key, path에 없는 url 요청시 500 에러대신 404 에러 뜨게 만듬
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
